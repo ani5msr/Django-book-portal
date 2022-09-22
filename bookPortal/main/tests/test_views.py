c0259b1b6089acf527ac28b6b016f30a3f006cb9
Anirudh Mishra <anirudh5mar@gmail.com>
2022-09-22 15:01:08 +0530

    Added forms

--- bookPortal/main/tests/test_views.py
+++ bookPortal/main/tests/test_views.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from main import forms
 class TestPage(TestCase):
  def test_home_page_works(self):
     response = self.client.get(reverse("home"))
@@ -11,3 +12,10 @@ class TestPage(TestCase):
     self.assertEqual(response.status_code, 200)
     self.assertTemplateUsed(response, 'aboutUsPage.html')
     self.assertContains(response, 'Book Portal')
+ def test_contact_us_page_works(self):
+    response = self.client.get(reverse("contact_us"))
+    self.assertEqual(response.status_code, 200)
+    self.assertTemplateUsed(response, 'main/contactForm.html')
+    self.assertContains(response, 'Book Portal')
+    self.assertIsInstance(
+    response.context["form"], forms.ContactForm)
--- bookPortal/main/views.py
+++ bookPortal/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.edit import FormView
+from main import forms
+class ContactUsView(FormView):
+    template_name = "contactForm.html"
+    form_class = forms.ContactForm
+    success_url = "/"
+    def form_valid(self, form):
+        form.send_mail()
+        return super().form_valid(form)
