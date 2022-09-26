0721f9a7f65bfa2fa317ae52dbd9786956bc3fc1
Anirudh Mishra <anirudh5mar@gmail.com>
2022-09-26 15:28:45 +0530

    Added json data for Product Tags

--- bookPortal/main/urls.py
+++ bookPortal/main/urls.py
@@ -2,8 +2,6 @@ from django.urls import path,include
 from django.views.generic import TemplateView
 from main import views
 from django.contrib import admin
-from django.conf.urls.static import static
-from django.conf import settings
 urlpatterns = [
  path(
  "about-us/",
@@ -20,6 +18,4 @@ urlpatterns = [
  views.ContactUsView.as_view(),
  name="contact_us",
  ),
- path('admin/', admin.site.urls),
- path('', include('main.urls')),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  
