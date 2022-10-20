d1ed8d0b551d2281ce3c6bf9cb790b9886fb28ae
Anirudh Mishra <anirudh5mar@gmail.com>
2022-10-20 16:54:43 +0530

    Added views for address update , delete

--- bookPortal/main/urls.py
+++ bookPortal/main/urls.py
@@ -34,4 +34,24 @@ urlpatterns = [
  ),
  path("signup/", views.SignupView.as_view(), name="signup"),
  path("login/",auth_views.LoginView.as_view(template_name="login.html",form_class=forms.AuthenticationForm,),name="login",),
+ path(
+ "address/",
+ views.AddressListView.as_view(),
+ name="address_list",
+ ),
+ path(
+ "address/create/",
+ views.AddressCreateView.as_view(),
+ name="address_create",
+ ),
+ path(
+ "address/<int:pk>/",
+ views.AddressUpdateView.as_view(),
+ name="address_update",
+ ),
+ path(
+ "address/<int:pk>/delete/",
+ views.AddressDeleteView.as_view(),
+ name="address_delete",
+ ),
 ]  
